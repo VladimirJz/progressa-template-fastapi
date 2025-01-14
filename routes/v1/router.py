66be961ffa7802b82ced185526cfa9d30b6d71eb
Vladimir Jz <vladimir.jz@hotmail.com>
2025-01-14 10:48:21 -0600

    docker template

--- routes/v1/router.py
+++
@@ -1,8 +0,0 @@
-from app.clientes.controller import router as router_clientes
-from fastapi import APIRouter
-
-
-main_router = APIRouter(
-    prefix="/api/v1"
-)
-main_router.include_router(router_clientes)
